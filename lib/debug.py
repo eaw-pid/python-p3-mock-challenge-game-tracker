790bc644b1b574cb7de4b5dd5db22d80282d11c7
Erin Wise <erinwise88@gmail.com>
2024-01-02 22:07:27 -0500

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,14 @@ from classes.many_to_many import Result
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
+    ben = Player('Ben')
+    tanner = Player('tanner')
+    game1 = Game('solitaire')
+    game2 = Game('monopoly')
+    result3 = Result(ben, game2, 500)
+    result2 = Result(ben, game2, 1500)
+    result1 = Result(ben, game1, 550)
+
+    
+
     ipdb.set_trace()
